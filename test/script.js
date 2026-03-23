a5443344c7a6b7e4edd20d75cc7bfdd2ecfda0b9
9649030-hash <9649030@graniteschools.org>
2026-03-23 08:25:48 -0700

    Update script.js

--- test/script.js
+++ test/script.js
@@ -3,5 +3,14 @@ createCanvas(600, 400);
 }
 
 function draw() {
-background(0);
+ background(#88B8FA);
+  drawRef();
+  histEvent(67,200,"Mar. 1918",true,"Harold Seeholzer, the founder of Beaver Mountain, buys his first pair of skis.");
+  histEvent(300,300,"Dec. 1939",false,"Logan Canyon is open year-round and the Ski Club decides to install a ski tow. ");
+  histEvent(450,200,"Dec. 1949",true,"A warming lodge was added. ");
+  histEvent(500,300,"Apr. 1950",false," A 2,700 foot T-bar is ordered.");
+  histEvent(600,200,"Dec. 1961",true," Harold and Luella call their young family together and \n form a corporation. They use horses to carry some of the supplies.");
+  histEvent(630,300,"Jan. 1963",false,"The family begins construction on Little Beaver double chair, the first beginning ski lift.");
+  histEvent(740,200,"Jan. 1970",true,"Harry’s Dream, a 4,600-foot double lift with 137 chairs opens. ");
+  histEvent(820,300,"May, 1986",false,"Beaver Mountain finally gets electricity after running off a generator for 47 years.");
 }
